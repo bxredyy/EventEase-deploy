1824bf72fb5805a8372f39e06f98f34c19744ee0
bxredyy <122664806+bxredyy@users.noreply.github.com>
2026-06-04 16:22:09 +0200

    Add StartDate and EndDate to Event model

--- cldv6211-g4-2026-part2-bxredyy-master/Models/Event.cs
+++ cldv6211-g4-2026-part2-bxredyy-master/Models/Event.cs
@@ -18,6 +18,17 @@ namespace EventEase.Models
         [Display(Name = "Description")]
         public string? Description { get; set; }
 
+        [Required(ErrorMessage = ""Event start date is required."")]
+        [DataType(DataType.Date)]
+        [Display(Name = ""Start Date"")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = ""Event end date is required."")]
+        [DataType(DataType.Date)]
+        [Display(Name = ""End Date"")]
+        public DateTime EndDate { get; set; }
+
+
         // POE Part 2A: Stored as an Azurite blob URL when uploaded.
         [Display(Name = "Event Image")]
         public string? ImageUrl { get; set; }
